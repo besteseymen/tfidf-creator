83f337bfd473b07c90708a849cd6c8160d4969d5
beste <beste.seymen@gmail.com>
2016-06-10 22:26:21 +0300

    changed small bug

--- src/com/computergodzilla/cosinesimilarity/Configuration.java
+++ src/com/computergodzilla/cosinesimilarity/Configuration.java
@@ -1,6 +1,8 @@
 // Configuration.java
 package com.computergodzilla.cosinesimilarity;
 
+//changed
+
 public class Configuration { 
     public static final String SOURCE_DIRECTORY_TO_INDEX = "/PATH_TO_THE_SOURCE";
     public static final String INDEX_DIRECTORY = "/PATH_TO_THE_INDEX";
